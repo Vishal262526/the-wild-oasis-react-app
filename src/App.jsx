9726518fa535b06b3ee44dd97ba3da5513a07a7d
Vishal Prajapati <vishal.kumar190601@gmail.com>
2023-12-10 17:48:47 +0530

    Global Styling and Variables completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import styled from "styled-components";
 import GlobalStyle from "./styles/GlobalStyles";
 
 function App() {
+
+  const Button = styled.button`
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+    font-weight: 500;
+    border: none;
+    border-radius: 7px;
+    background-color: var(--color-brand-600);
+    color: var(--color-brand-50);
+    cursor: pointer;
+  `;
+
   return <>
   
     <GlobalStyle />
 
     <h1>Hello World</h1>
+    <Button>Hello World</Button>
   </>
 }
 
